2ad9f96cd0d7a0adcece026136ff8db243b53704
Yongjun Park <yopark@student.42seoul.kr>
2023-06-03 17:17:57 +0900

    🎉 feat: tailwindcss

---
+++ src/pages/info.tsx
@@ -0,0 +1,43 @@
+import Text from '@/components/Text'
+import { graphql } from 'gatsby'
+type InfoPageProps = {
+  data: {
+    site: {
+      siteMetadata: {
+        title: string
+        description: string
+        author: string
+      }
+    }
+  }
+}
+
+const InfoPage = ({
+  data: {
+    site: {
+      siteMetadata: { title, description, author },
+    },
+  },
+}: InfoPageProps) => {
+  return (
+    <div className="font-bold underline">
+      <Text text={title} />
+      <Text text={description} />
+      <Text text={author} />
+    </div>
+  )
+}
+
+export default InfoPage
+
+export const metadataQuery = graphql`
+  {
+    site {
+      siteMetadata {
+        title
+        description
+        author
+      }
+    }
+  }
+`

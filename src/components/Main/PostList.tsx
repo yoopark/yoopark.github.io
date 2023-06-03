f973f657e28156a7fdd4559832bc52b4b271fac4
Yongjun Park <yopark@student.42seoul.kr>
2023-06-03 22:00:54 +0900

    🐛 fix: infine scroll 새로 구현

--- src/components/Main/PostList.tsx
+++ src/components/Main/PostList.tsx
@@ -1,5 +1,6 @@
 import type { PostType } from '@/types/PostItem';
-import useInfiniteScroll from '@hooks/useInfiniteScroll';
+import { useInfiniteScroll } from '@hooks/useInfiniteScroll';
+import { useEffect, useMemo, useState } from 'react';
 import { PostItem } from './PostItem';
 
 type PostListProps = {
@@ -7,17 +8,47 @@ type PostListProps = {
   posts: PostType[];
 };
 
+const NUMBER_OF_ITEMS_PER_PAGE = 10;
+
 export const PostList = ({ selectedCategory, posts }: PostListProps) => {
-  const { containerRef, postList } = useInfiniteScroll(selectedCategory, posts);
+  const { ref, isVisible } = useInfiniteScroll();
+
+  const [count, setCount] = useState<number>(1);
+
+  const postListByCategory = useMemo<PostType[]>(
+    () =>
+      posts.filter(
+        ({
+          node: {
+            frontmatter: { categories },
+          },
+        }) =>
+          selectedCategory !== 'All'
+            ? categories.includes(selectedCategory)
+            : true,
+      ),
+    [selectedCategory],
+  );
+
+  const postList = useMemo<PostType[]>(
+    () => postListByCategory.slice(0, count * NUMBER_OF_ITEMS_PER_PAGE),
+    [count, selectedCategory],
+  );
+
+  useEffect(() => {
+    if (!isVisible) return;
+
+    setCount((value) => value + 1);
+  }, [isVisible]);
+
+  useEffect(() => setCount(1), [selectedCategory]);
 
   return (
-    <div
-      ref={containerRef}
-      className="flex flex-col gap-6 max-w-screen-sm w-full m-auto"
-    >
+    <div className="flex flex-col gap-6 max-w-screen-sm w-full m-auto">
       {postList.map(({ node: { id, frontmatter } }) => (
         <PostItem {...frontmatter} link="/" key={id} />
       ))}
+      <div ref={ref} className="w-full h-1 bg-transparent"></div>
     </div>
   );
 };
--- src/hooks/useInfiniteScroll.tsx
+++ src/hooks/useInfiniteScroll.tsx
@@ -1,58 +1,9 @@
-import type { PostType } from '@/types/PostItem';
-import { MutableRefObject, useEffect, useMemo, useRef, useState } from 'react';
+import { useRef } from 'react';
+import { useIntersectionObserver } from './useIntersectionObserver';
 
-const NUMBER_OF_ITEMS_PER_PAGE = 10;
-
-const useInfiniteScroll = (selectedCategory: string, posts: PostType[]) => {
-  const containerRef: MutableRefObject<HTMLDivElement | null> =
-    useRef<HTMLDivElement>(null);
-
-  const [count, setCount] = useState<number>(1);
-
-  const postListByCategory = useMemo<PostType[]>(
-    () =>
-      posts.filter(
-        ({
-          node: {
-            frontmatter: { categories },
-          },
-        }) =>
-          selectedCategory !== 'All'
-            ? categories.includes(selectedCategory)
-            : true,
-      ),
-    [selectedCategory],
-  );
-
-  const observer = new IntersectionObserver((entries, observer) => {
-    if (!entries[0].isIntersecting) return;
-
-    setCount((value) => value + 1);
-    observer.disconnect();
-  });
-
-  useEffect(() => setCount(1), [selectedCategory]);
-
-  useEffect(() => console.log(count), [count]);
-
-  useEffect(() => {
-    if (
-      NUMBER_OF_ITEMS_PER_PAGE * count >= postListByCategory.length ||
-      containerRef.current === null ||
-      containerRef.current.children.length === 0
-    )
-      return;
-
-    // 기준 : 마지막 요소의 50% 이상이 보이면 다음 페이지를 로드한다.
-    observer.observe(
-      containerRef.current.children[containerRef.current.children.length - 1],
-    );
-  }, [count, selectedCategory]);
-
-  return {
-    containerRef,
-    postList: postListByCategory.slice(0, count * NUMBER_OF_ITEMS_PER_PAGE),
-  };
+export const useInfiniteScroll = (threshold = 0.5) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const entry = useIntersectionObserver(ref, { threshold });
+  const isVisible = !!entry?.isIntersecting;
+  return { ref, isVisible };
 };
-
-export default useInfiniteScroll;

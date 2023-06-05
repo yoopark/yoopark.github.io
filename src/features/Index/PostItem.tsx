5b39839ad1c232509d126f893d135736579bb82c
Yongjun Park <yopark@student.42seoul.kr>
2023-06-05 21:02:53 +0900

    💄 feat: 반응형 고려

--- src/features/Index/PostItem.tsx
+++ src/features/Index/PostItem.tsx
@@ -7,7 +7,7 @@ export const PostItem = ({ title, date, link }: PostItemProps) => {
   return (
     <li className="vstack gap-1.5">
       <Link to={link}>
-        <h3 className="text-2xl font-bold">{title}</h3>
+        <h3 className="text-xl font-bold md:text-2xl">{title}</h3>
       </Link>
       <p className="text-sm">{date}</p>
     </li>
--- src/features/Post/PostHeader.tsx
+++ src/features/Post/PostHeader.tsx
@@ -6,8 +6,10 @@ type PostHeaderProps = {
 
 export const PostHeader = ({ title, date, categories }: PostHeaderProps) => {
   return (
-    <div className="justify-center w-full gap-5 vstack h-60">
-      <h1 className="text-4xl font-extrabold text-center">{title}</h1>
+    <div className="justify-center w-full h-40 gap-5 vstack md:h-60">
+      <h1 className="text-2xl font-extrabold text-center md:text-4xl">
+        {title}
+      </h1>
       <span>
         {date} / {categories.join(', ')}
       </span>
